7dd0e76e0ebfea77ad635524bc86fa7954fe9142
waterlog98-code <waterlog98@gmail.com>
2026-05-26 15:28:22 -0700

    Update codebase

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import PLAYER_RADIUS, SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
+    # Initialize screen and clock
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+     # Calculate middle coordinates
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    
+    # Instantiate the Player object in the center
+    player = Player(x, y, PLAYER_RADIUS)
+
     while True:
         log_state()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-    screen.fill("black")
-    pygame.display.flip()
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}! Screen width: {SCREEN_WIDTH}, Screen height: {SCREEN_HEIGHT}")
+        screen.fill("black")
+        player.draw(screen)
+        
+        clock.tick(60)
+        dt = clock.tick(60) / 1000.0
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        print(f"Starting Asteroids with pygame version: {pygame.version.ver}! Screen width: {SCREEN_WIDTH}, Screen height: {SCREEN_HEIGHT}")
 
 
 
---
+++ player.py
@@ -0,0 +1,21 @@
+from constants import PLAYER_RADIUS
+import pygame
+from circleshape import CircleShape
+
+class Player(CircleShape):
+    def __init__(self, x, y, PLAYER_RADIUS):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    # in the Player class
+    def triangle(self) -> list[pygame.Vector2]:
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        # This draws the player's triangle on the screen
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
