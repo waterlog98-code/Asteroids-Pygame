656156eb2b046cbe5a3704fdc53eeed5fee19f91
waterlog98-code <waterlog98@gmail.com>
2026-05-31 14:49:01 -0700

    Added asteroid splitting and asteroid destruction when smallest asteroid is hit

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 from circleshape import CircleShape
 from constants import *
 import pygame
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float) -> None:
@@ -11,3 +13,25 @@ class Asteroid(CircleShape):
 
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
+    
+    def split(self) -> list["Asteroid"]:
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            self.kill()
+            return []
+
+        log_event("asteroid_split")
+        angle1 = random.uniform(-50, -20)
+        angle2 = random.uniform(20, 50)
+
+        speed = self.velocity.length() or 100
+        v1 = pygame.math.Vector2(1, 0).rotate(angle1) * speed
+        v2 = pygame.math.Vector2(1, 0).rotate(angle2) * speed
+
+        child_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, child_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, child_radius)
+        asteroid1.velocity = v1
+        asteroid2.velocity = v2
+        self.kill()
+
+        return [asteroid1, asteroid2]
--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
             for shot in shots:
                 if asteroid.collides_with(shot):
                     log_event("asteroid_shot")
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
                 
 
